8ad9c9d671dea112866d36c8c1869be258875050
James <jimibue@gmail.com>
2022-01-18 11:10:49 -0800

    some chnage

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -10,12 +10,13 @@ class Api::MemesController < ApplicationController
             begin
                 cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true, resources_type: 'auto')
             rescue => e
+                puts "HERE!!!!"
                 debugger
                 render json: {errors: e}, status: 435
                 return
             end
         end
-        puts "HERE!!!!"
+      
         puts "HERE!!!!"
         # succefully added to cloudinary try to save Meme to our db
         meme = Meme.new(title: params[:title], image_url:cloud_image['secure_url'] )
